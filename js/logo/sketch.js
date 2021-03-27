f509b506c9f0febdc04b8fbd71f5358dadb5762f
Sergio Alejandro Vargas <savargasqu+git@unal.edu.co>
2021-03-27 17:24:54 -0500

    Update sketches
    - Indent at 4 spaces
    - Update colors
    - organize a little

--- js/logo/sketch.js
+++ js/logo/sketch.js
@@ -1,49 +1,53 @@
 // p5.js sketch to draw the logo
 
+let fg, bg;
+
 function setup() {
-  createCanvas(640, 320);
-  colorMode(HSL);
+    createCanvas(640, 320);
+    colorMode(HSL);
 
-  const bg = color(30, 10, 15);
-  const fg = color('#E2DBD5');
+    bg = color(30, 10, 15);
+    fg = color('#E2DBD5');
 
-  background(bg);
-  stroke(fg);
-  strokeWeight(2);
-  fill(fg);
+    background(bg);
+    stroke(fg);
+    strokeWeight(2);
+    fill(fg);
 
-  textFont('Noto Serif');
-  textStyle(BOLDITALIC);
-  textAlign(CENTER);
-  textSize(80);
-  text('meλanʒe', width / 2, height / 2);
+    textFont('Noto Serif');
+    textStyle(BOLDITALIC);
+    textAlign(CENTER);
+    textSize(80);
+    text('meλanʒe', width / 2, height / 2);
 
-  line(0, height *2 / 3, width, height * 2 / 3);
-  draw_colors();
+    line(0, height *2 / 3, width, height * 2 / 3);
+    draw_colors();
 
-  noLoop();
-  //saveCanvas('melange-logo');
+    noLoop();
+    //saveCanvas('melange-logo');
 }
 
 function draw_colors() {
-  draw_hsl(-10,  60, 60);
+    draw_hsl(-10, 60, 60);
+    //draw_hsl( 10, 90, 70);
+    draw_hsl( 30, 60, 50);
+    //draw_hsl( 40, 60, 50);
+    draw_hsl( 40, 90, 70);
 
-  draw_hsl( 30,  60, 50);
-  draw_hsl( 40, 100, 70);
+    draw_hsl(100, 40, 60);
+    draw_hsl(150, 40, 50);
+    draw_hsl(180, 20, 60);
 
-  draw_hsl(100,  40, 60);
-  draw_hsl(150,  40, 50);
-  draw_hsl(180,  20, 60);
+    draw_hsl(225, 30, 70);
+    draw_hsl(270, 30, 60);
+    draw_hsl(310, 40, 70);
 
-  draw_hsl(225,  30, 70);
-  draw_hsl(270,  30, 60);
-  draw_hsl(310,  40, 70);
 }
 
 function draw_hsl(h, s, l) {
-  fill(30, 10, 15); // bg
-  stroke(h, s, l);
-  strokeWeight(2);
-  const x = map(h, -30, 330, 0, width);
-  circle(x, height * 2 / 3, 2000 / l);
+    fill(bg);
+    stroke(h, s, l);
+    strokeWeight(2);
+    const x = map(h, -30, 330, 0, width);
+    circle(x, height * 2 / 3, 2000 / l);
 } 

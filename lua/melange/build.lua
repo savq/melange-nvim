861303dd6788887413661e9bdf6c96ebe1fc4664
Sergio Alejandro Vargas <savargasqu+git@unal.edu.co>
2022-11-05 15:43:01 -0500

    Rename bright colors

--- lua/melange/build.lua
+++ lua/melange/build.lua
@@ -36,32 +36,32 @@ local function get_palette24(variant)
   local colors = require(string.format('melange/palettes/%s', bg))
   -- stylua: ignore
   return {
-    bg           = colors.a.bg,
-    fg           = colors.a.fg,
-    dark_black   = colors.a.bg,
-    dark_red     = colors.d.red,
-    dark_green   = colors.d.green,
-    dark_yellow  = colors.d.yellow,
-    dark_blue    = colors.d.blue,
-    dark_magenta = colors.d.magenta,
-    dark_cyan    = colors.d.cyan,
-    dark_white   = colors.a.com,
-    black        = colors.a.overbg,
-    red          = colors.c.red,
-    green        = colors.c.green,
-    yellow       = colors.c.yellow,
-    blue         = colors.c.blue,
-    magenta      = colors.c.magenta,
-    cyan         = colors.c.cyan,
-    white        = colors.a.faded,
-    brblack      = colors.a.sel,
-    brred        = colors.b.red,
-    brgreen      = colors.b.green,
-    bryellow     = colors.b.yellow,
-    brblue       = colors.b.blue,
-    brmagenta    = colors.b.magenta,
-    brcyan       = colors.b.cyan,
-    brwhite      = colors.a.fg,
+    bg             = colors.a.bg,
+    fg             = colors.a.fg,
+    dark_black     = colors.a.bg,
+    dark_red       = colors.d.red,
+    dark_green     = colors.d.green,
+    dark_yellow    = colors.d.yellow,
+    dark_blue      = colors.d.blue,
+    dark_magenta   = colors.d.magenta,
+    dark_cyan      = colors.d.cyan,
+    dark_white     = colors.a.com,
+    black          = colors.a.overbg,
+    red            = colors.c.red,
+    green          = colors.c.green,
+    yellow         = colors.c.yellow,
+    blue           = colors.c.blue,
+    magenta        = colors.c.magenta,
+    cyan           = colors.c.cyan,
+    white          = colors.a.faded,
+    bright_black   = colors.a.sel,
+    bright_red     = colors.b.red,
+    bright_green   = colors.b.green,
+    bright_yellow  = colors.b.yellow,
+    bright_blue    = colors.b.blue,
+    bright_magenta = colors.b.magenta,
+    bright_cyan    = colors.b.cyan,
+    bright_white   = colors.a.fg,
   }
 end
 
@@ -108,14 +108,14 @@ colors:
     cyan:    '$cyan'
     white:   '$white'
   bright:
-    black:   '$brblack'
-    red:     '$brred'
-    green:   '$brgreen'
-    yellow:  '$bryellow'
-    blue:    '$brblue'
-    magenta: '$brmagenta'
-    cyan:    '$brcyan'
-    white:   '$brwhite'
+    black:   '$bright_black'
+    red:     '$bright_red'
+    green:   '$bright_green'
+    yellow:  '$bright_yellow'
+    blue:    '$bright_blue'
+    magenta: '$bright_magenta'
+    cyan:    '$bright_cyan'
+    white:   '$bright_white'
 ]]
 
 terminals.kitty.template = [[
@@ -123,13 +123,13 @@ background $bg
 foreground $fg
 cursor     $fg
 url_color  $blue
-selection_background    $brblack
+selection_background    $bright_black
 selection_foreground    $fg
 tab_bar_background      $black
 active_tab_background   $black
 active_tab_foreground   $yellow
 inactive_tab_background $black
-inactive_tab_foreground $brwhite
+inactive_tab_foreground $bright_white
 color0  $black
 color1  $red
 color2  $green
@@ -139,14 +139,14 @@ color5  $magenta
 color6  $cyan
 color7  $white
 
-color8  $brblack
-color9  $brred
-color10 $brgreen
-color11 $bryellow
-color12 $brblue
-color13 $brmagenta
-color14 $brcyan
-color15 $brwhite
+color8  $bright_black
+color9  $bright_red
+color10 $bright_green
+color11 $bright_yellow
+color12 $bright_blue
+color13 $bright_magenta
+color14 $bright_cyan
+color15 $bright_white
 ]]
 
 terminals.terminator.template = [=[
@@ -154,7 +154,7 @@ terminals.terminator.template = [=[
     background_color = "$bg"
     cursor_color = "$fg"
     foreground_color = "$fg"
-    palette = "$black:$red:$green:$yellow:$blue:$magenta:$cyan:$white:$brblack:$brred:$brgreen:$bryellow:$brblue:$brmagenta:$brcyan:$brwhite"
+    palette = "$black:$red:$green:$yellow:$blue:$magenta:$cyan:$white:$bright_black:$bright_red:$bright_green:$bright_yellow:$bright_blue:$bright_magenta:$bright_cyan:$bright_white"
 ]=]
 
 terminals.termite.template = [[
@@ -169,15 +169,15 @@ color4     = $blue
 color5     = $magenta
 color6     = $cyan
 color7     = $white
-color8     = $brblack
-color9     = $brred
-color10    = $brgreen
-color11    = $bryellow
-color12    = $brblue
-color13    = $brmagenta
-color14    = $brcyan
-color15    = $brwhite
-highlight  = $brblack
+color8     = $bright_black
+color9     = $bright_red
+color10    = $bright_green
+color11    = $bright_yellow
+color12    = $bright_blue
+color13    = $bright_magenta
+color14    = $bright_cyan
+color15    = $bright_white
+highlight  = $bright_black
 ]]
 
 terminals.wezterm.template = [[
@@ -187,10 +187,10 @@ background    = "$bg"
 cursor_bg     = "$fg"
 cursor_border = "$fg"
 cursor_fg     = "$bg"
-selection_bg  = "$brblack"
+selection_bg  = "$bright_black"
 selection_fg  = "$fg"
 ansi = ["$black", "$red", "$green", "$yellow", "$blue", "$magenta", "$cyan", "$white"]
-brights = ["$brblack", "$brred", "$brgreen", "$bryellow", "$brblue", "$brmagenta", "$brcyan", "$brwhite"]
+brights = ["$bright_black", "$bright_red", "$bright_green", "$bright_yellow", "$bright_blue", "$bright_magenta", "$bright_cyan", "$bright_white"]
 ]]
 
 return {
